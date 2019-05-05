42b38e87472adc31048586b6b03086c675f5d4cf
Christian Braune <c.braune@cominform.de>
2019-05-05 22:37:29 +0200

    Add coupon component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomeComponent} from "./home/home.component";
 import {CheckoutComponent} from "./checkout/checkout.component";
+import {CouponComponent} from "./coupon/coupon.component";
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'home', component: HomeComponent},
+  {path: 'coupon', component: CouponComponent},
   {path: 'checkout', component: CheckoutComponent}
 ];
 
